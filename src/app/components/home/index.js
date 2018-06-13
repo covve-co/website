ac1302055a7bcba37238ebba2d5e834faca16d45
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-06-13 12:48:56 +0800

    Add tracking for waves

--- src/app/components/home/index.js
+++ src/app/components/home/index.js
@@ -4,6 +4,16 @@ import waves from './waves.svg';
 import logo from './logo.svg';
 
 export default class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      waves: -300
+    };
+  }
+  _onMouseMove(e) {
+    this.setState({ waves: -300 + e.screenY / 20 });
+  }
+
   render() {
     return (
       <div style={{ display: 'flex', flex: 5, flexDirection: 'row' }}>
@@ -12,7 +22,7 @@ export default class Home extends React.Component {
           style={{ flex: 2, display: 'flex', flexDirection: 'column' }}
         >
           <img class="logo" style={{ flex: 1 }} src={logo} alt="this no work" />
-          <div style={{ flex: 5 }}>
+          <div style={{ flex: 5 }} onMouseMove={this._onMouseMove.bind(this)}>
             <HomeButton link="/about" title="About" />
             <HomeButton link="/work" title="Work" />
             <HomeButton link="/contact" title="Contact" />
@@ -20,7 +30,17 @@ export default class Home extends React.Component {
           </div>
         </div>
         <div class="home-right" style={{ flex: 1 }}>
-          <img class="waves" src={waves} alt="this no work" />
+          <img
+            id="waves"
+            class="waves"
+            src={waves}
+            alt="this no work"
+            style={{
+              position: 'absolute',
+              top: this.state.waves,
+              height: '150vh'
+            }}
+          />
         </div>
       </div>
     );
